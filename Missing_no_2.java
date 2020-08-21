0e576886746110c0ce73a9b7d2db02b6d220eead
Nisha556 <shandilyanisha1@gmail.com>
2020-08-21 23:17:05 +0530

    new prog

---
+++ Missing_no_2.java
@@ -0,0 +1,29 @@
+package Amazon;
+//Java program to find missing Number 
+public class Missing_no_2 {
+	// Java implementation 
+
+
+		// a represents the array 
+		// n : Number of elements in array a 
+		static int getMissingNo(int a[], int n) 
+		{ 
+			int total = 1; 
+			for (int i = 2; i <= (n + 1); i++) 
+			{ 
+				total += i; 
+				total -= a[i - 2]; 
+			} 
+			return total; 
+		} 
+
+		// Driver Code 
+		public static void main(String[] args) 
+		{ 
+			int[] arr = { 1, 3, 5 }; 
+			System.out.println(getMissingNo(arr, arr.length)); 
+		} 
+	} 
+
+	// This post is contributed 
+	// by Vivek Kumar Singh 
